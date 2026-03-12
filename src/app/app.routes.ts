aef92c44acffd11023788136ae224a8f8879f088
galaxiasssss <vitororbe@gmail.com>
2026-03-12 16:43:30 -0300

    tela da homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Reserva } from './pages/reserva/reserva';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'reserva', component: Reserva }
+];
